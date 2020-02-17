122d76e64cbb505f5fef0713f84ca1a1d365381b
Neal Fultz <nfultz@gmail.com>
2020-02-17 23:36:44 +0000

    Fix precision issues, fill in doc, don't retain FEINs of organizations

--- R/calculate.R
+++ R/calculate.R
@@ -6,18 +6,39 @@ OGP_CATEGORIES <- c(0, 17500, 100000, 1500000, 40000000, Inf)
 
 OGP.2013 <- function(B) {
 
-  category <- cut(B, OGP_CATEGORIES, FALSE)
+  Vectorize(function(budgetSize){
+    if(budgetSize < 17500) {
+      grant = 7500;
+    }
+    else if (budgetSize >= 17500 && budgetSize <= 100000) {
+      grant = 606.0606 + 0.3939394 * budgetSize;
+    }
+    else if (budgetSize >= 100000 && budgetSize <= 1500000) {
+      grant = 36250 + 0.0375 * budgetSize;
+    }
+    else if (budgetSize >= 1500000 && budgetSize <= 40000000) {
+      grant = 84415.5844 + 0.0053896 * budgetSize;
+    }
+    else {
+      grant = 300000;
+    }
 
-  alpha <- c(7500, 606, 36250, 84415.58, 300000)
-  beta  <- c(0, .3939, .0375, .0054, 0)
+    grant
+  })(B)
+  #category <- cut(B, OGP_CATEGORIES, FALSE)
 
-  alpha[category] + beta[category] * B
+  #alpha <- c(7500, 606, 36250, 84415.58, 300000)
+  #beta  <- c(0, .3939, .0375, .0054, 0)
+
+  #alpha[category] + beta[category] * B
 }
 
 OGP.2013_forward <- function(B) {
 
+  c1 <- 0.3939394
+  c2 <- 0.0053896
   categories <- c(0,        1,     7500, 17500, 50000, 100000, 1500000, 10000000, 40000000)
-  beta <-       c(7500,     0,        0, .3939, .3939,  .0375,   .0054,    .0054,        0)
+  beta <-       c(7500,     0,        0,    c1,    c1,  .0375,      c2,       c2,        0)
 
 
   b <- outer(B, categories, `-`); b
@@ -25,14 +46,18 @@ OGP.2013_forward <- function(B) {
   b[] <- pmax(b, 0); b
   b <- sweep(b, MARGIN=2, STATS = c(diff(categories), Inf), FUN=pmin)
 
-  cbind(B, round(b), new=b %*% beta, old=OGP.2013(B))
+  drop(b %*% beta)
 }
 
 
 Award <- function(Budget, Grant100Formula, Scores, TotalBudget=4500000, ...) {
 
   Grant100 <- Grant100Formula(Budget)
-  Awards <-  Grant100 * Scores
-  TotalBudget * Awards / sum(Awards)
+  Awards   <-  Grant100 * Scores
+  mod      <- TotalBudget / sum(Awards)
+
+  Final <- Awards * mod
+
+  structure(data.frame(..., Grant100, Awards, Final), mod=mod)
 }
 
--- data/allocations.R
+++ data/allocations.R
@@ -31,6 +31,16 @@ local({
     n <- sum(!is.na(allocations$OGP_Budget_Category))
   }
 
+  # Drop unneeded columns
+  allocations[c("FEIN", "Request", "Cutoff",
+                "RawScore", "Proposed_100_XScore", "Proposed_50",
+                "Rounded_Allocation_Final Award", "Funding_Year",
+                "Score", "Total_Grant_Award", "Prior_Budget_Size", "Variance_Value",
+                "NOTES", "Decrease_award_per", "Decrease_score", "Budget_Decrease",
+                "Project_Description", "Office_Location", "District_Most_Activity",
+                "District", "decreased_score_q", "tail")] <- NULL
+
+
   tbl <- tabulizer::extract_tables("ogp_1920_list_of_grantees_for_web.pdf")
   nms <- setNames(nm=tbl[[1]][1,])
   nms["Grantee"] = "Organization" # To match allocations spreadsheet for merging
