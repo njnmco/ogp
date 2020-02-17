d18c6fd88305d215b5fad60caf4eae4d0834e8f7
Neal Fultz <nfultz@gmail.com>
2020-02-17 22:30:34 +0000

    back transform pw linear into standard repr; add a couple extra categories.

--- R/calculate.R
+++ R/calculate.R
@@ -13,3 +13,26 @@ OGP.2013 <- function(B) {
 
   alpha[category] + beta[category] * B
 }
+
+OGP.2013_forward <- function(B) {
+
+  categories <- c(0,        1,     7500, 17500, 50000, 100000, 1500000, 10000000, 40000000)
+  beta <-       c(7500,     0,        0, .3939, .3939,  .0375,   .0054,    .0054,        0)
+
+
+  b <- outer(B, categories, `-`); b
+
+  b[] <- pmax(b, 0); b
+  b <- sweep(b, MARGIN=2, STATS = c(diff(categories), Inf), FUN=pmin)
+
+  cbind(B, round(b), new=b %*% beta, old=OGP.2013(B))
+}
+
+
+Award <- function(Budget, Grant100Formula, Scores, TotalBudget=4500000, ...) {
+
+  Grant100 <- Grant100Formula(Budget)
+  Awards <-  Grant100 * Scores
+  TotalBudget * Awards / sum(Awards)
+}
+
